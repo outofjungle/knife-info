d9e94b78847b731468519c29e93f94cd095799a1
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-11-21 03:54:05 -0800

    proof of concept output

--- knife-info.gemspec
+++ knife-info.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
   
   spec.add_dependency 'chef'
   spec.add_development_dependency 'bundler', '~> 1.3'
-  spec.add_development_dependency "rake"
-  
+  spec.add_development_dependency 'rake'
+
   spec.license = 'MIT'
 end
--- lib/knife/info.rb
+++ lib/knife/info.rb
@@ -1,10 +1,24 @@
 require 'chef/knife'
+require 'uri'
 
 class Chef
   class Knife
-    class Info
+    class Info < Knife
+
       def self.print
-        puts "hello world"
+        
+        knife = Chef::Knife::Info.new
+        knife.configure_chef
+        uri = URI(knife.server_url)
+
+        puts uri.host
+        puts uri.path
+        puts knife.username
+        puts Chef::Knife.locate_config_file
+      end
+
+      def initialize(argv=[])
+        super()
       end
     end
   end
