1f40313ac50bbbc22864bc5901f101c0e6671460
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-12-04 15:41:17 -0800

    one line status

--- lib/knife/info.rb
+++ lib/knife/info.rb
@@ -5,20 +5,29 @@ class Chef
   class Knife
     class Info < Knife
 
-      def self.print
-        
-        knife = Chef::Knife::Info.new
-        knife.configure_chef
-        uri = URI(knife.server_url)
-
-        puts uri.host
-        puts uri.path
-        puts knife.username
-        puts Chef::Knife.locate_config_file
+      def self.info
+        knife_info = Chef::Knife::Info.new
+        knife_info.pretty_print
       end
 
+      attr_reader :host, :organization, :user, :config_file_location
       def initialize(argv=[])
         super()
+        configure_chef
+
+        @config_file_location = Chef::Knife.locate_config_file
+        @user = username
+
+        uri = URI(server_url)
+        %r(^(?<host>[a-zA-Z0-9-]+)\..*$) =~ uri.host
+        @host = host || ''
+
+        %r(.*/(?<org>.*)$) =~ uri.path
+        @organization = org || ''
+      end
+
+      def pretty_print
+        puts @user + '@' + @host + '/' + @organization
       end
     end
   end
